98a8a17adeb5ab8f6c500e130a31ec15707772d5
laldoroty <laldoroty@tamu.edu>
2021-06-25 11:53:33 -0500

    added the tests folder back in lol

---
+++ tests/unittest.py
@@ -0,0 +1,14 @@
+from astropy.table import Table
+from snlcpy import fpca
+
+
+def test_getpctemplates():
+    '''
+    Tests that the FPCA templates are pulled.
+    '''
+    filters = ['B', 'V', 'R', 'I', 'vague']
+    for filt in filters:
+        PCs = fpca.get_pctemplates(filt)
+        assert len(PCs) == 5
+
+test_getpctemplates()
