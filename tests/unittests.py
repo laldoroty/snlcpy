2ea33793446fa9c6256aeb1691a481d670ec9453
jiaweny <37666860+jiaweny@users.noreply.github.com>
2021-06-24 22:35:10 -0500

    update unittest

--- tests/unittests.py
+++ tests/unittests.py
@@ -2,13 +2,15 @@ import pytest
 from astropy.table import Table
 from snlcpy import fpca
 
+
 def test_getpctemplates():
     '''
     Tests that the FPCA templates are pulled.
     '''
-    filters = ['B','V','R','I','vague']
+    filters = ['B', 'V', 'R', 'I', 'vague']
     for filt in filters:
         PCs = fpca.get_pctemplates(filt)
         assert len(PCs) == 5
+
 
 test_getpctemplates()
